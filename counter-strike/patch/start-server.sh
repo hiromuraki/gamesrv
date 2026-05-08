1ba953899114994c106f4385211783bdab6f395b
HiroMuraki <419734782@qq.com>
2026-05-08 18:06:08 +0800

    修正部分配置错误

--- counter-strike/patch/start-server.sh
+++ counter-strike/patch/start-server.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+
+
 ./hlds_run -game cstrike \
     -insecure \
     -nomaster \
     -strictportbind \
     -ip 0.0.0.0 \
     -port 27015 \
-    +exec "gamemode_deathmatch.cfg" \
+    +maxplayers 12 \
     +map de_dust2
