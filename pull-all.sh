22b7cdd2870036df7ef439b3d338d40f166d0805
HiroMuraki <419734782@qq.com>
2026-05-10 16:45:54 +0800

    添加MC的格雷科技新视野服务器

---
+++ pull-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+podman pull ghcr.io/hiromuraki/counter-strike:latest
+podman pull ghcr.io/hiromuraki/counter-strike-condition-zero:latest
+podman pull ghcr.io/hiromuraki/counter-strike-source:latest
+podman pull ghcr.io/hiromuraki/minecraft-gtnh:latest
+podman pull ghcr.io/hiromuraki/terraria:latest
+podman pull ghcr.io/hiromuraki/warfork:latest
