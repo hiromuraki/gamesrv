2628575698e2f0c0f2658c66c6f4b5318983725b
HiroMuraki <419734782@qq.com>
2026-05-30 16:50:05 +0800

    更新mod

--- pull-all.sh
+++ pull-all.sh
@@ -6,6 +6,7 @@ podman pull ghcr.io/hiromuraki/counter-strike-condition-zero:dm
 podman pull ghcr.io/hiromuraki/counter-strike-condition-zero:versus
 podman pull ghcr.io/hiromuraki/counter-strike-source:versus
 podman pull ghcr.io/hiromuraki/minecraft-gtnh:latest
+podman pull ghcr.io/hiromuraki/minecraft-casual:1.21.1-latest
 podman pull ghcr.io/hiromuraki/terraria:latest
 podman pull ghcr.io/hiromuraki/terraria-tshock:latest
 podman pull ghcr.io/hiromuraki/warfork:latest
